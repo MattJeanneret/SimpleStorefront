479509b6829ee26c91e230008f9a1099e6b39486
unknown <matt.jeanneret@alumni.wfu.edu>
2019-09-16 19:45:09 -0400

    Add logout function. Add jsdocs

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -4,10 +4,25 @@ class HeaderComponent extends React.Component {
         super(props);
         this.isLoggedIn = false;
         this.login = this.login.bind(this);
+        this.logout = this.logout.bind(this)
     }
 
+    /**
+     * @name login
+     * @description logs in the user
+     * @returns {void}
+     */
     login() {
         this.isLoggedIn = true;
+    }
+
+    /**
+     * @name logout
+     * @description logs out the user
+     * @returns void
+     */
+    logout() {
+        this.isLoggedIn = false;
     }
     render() {
         //TODO add logic to check if the user is logged in or out and changed the name and function on the button
