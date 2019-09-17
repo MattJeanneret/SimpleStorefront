5e4fc53775e911d4cd07c74906f268524fc63492
unknown <matt.jeanneret@alumni.wfu.edu>
2019-09-16 20:47:11 -0400

    Add toggle for log in and out

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 export default class Header extends Component {
     constructor(props) {
         super(props);
-        this.isLoggedIn = false;
+
+        this.state = {isLoggedIn: false};
         this.login = this.login.bind(this);
         this.logout = this.logout.bind(this)
     }
@@ -14,7 +15,7 @@ export default class Header extends Component {
      * @returns {void}
      */
     login() {
-        this.isLoggedIn = true;
+        this.setState({isLoggedIn: true});
     }
 
     /**
@@ -23,16 +24,16 @@ export default class Header extends Component {
      * @returns void
      */
     logout() {
-        this.isLoggedIn = false;
+        this.setState({isLoggedIn: false});
     }
     render() {
         //TODO add logic to check if the user is logged in or out and changed the name and function on the button
-        const isLoggedIn = this.isLoggedIn;
+        const isLoggedIn = this.state.isLoggedIn;
         let button;
         if (isLoggedIn) {
-            button = <button class="login" onclick={this.logout}>Logout</button>
+            button = <button className="login" onClick={this.logout}>Logout</button>
         } else {
-            button = <button class="login" onclick={this.login}>Login</button>
+            button = <button className="login" onClick={this.login}>Login</button>
         }
         return (
             <div id="store-header">{button}</div>
