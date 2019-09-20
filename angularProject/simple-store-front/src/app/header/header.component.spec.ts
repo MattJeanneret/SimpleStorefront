4f99a1b00988626dd31bde00e2358f4fb5962e19
unknown <matt.jeanneret@alumni.wfu.edu>
2019-09-20 13:09:53 -0400

    Add test cases for header

--- angularProject/simple-store-front/src/app/header/header.component.spec.ts
+++ angularProject/simple-store-front/src/app/header/header.component.spec.ts
@@ -5,10 +5,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { LoginService } from '../services/loginService/login.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let loginService: LoginService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,8 +19,10 @@ describe('HeaderComponent', () => {
       imports: [
         MatToolbarModule,
         MatDividerModule,
-        MatMenuModule
-      ]
+        MatMenuModule,
+        BrowserAnimationsModule
+      ],
+      providers: [LoginService]
     })
     .compileComponents();
   }));
@@ -26,9 +31,32 @@ describe('HeaderComponent', () => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    loginService = TestBed.get(LoginService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call the login service', async(() => {
+    const loginSpy = spyOn(loginService, 'login').and.stub();
+    const button = fixture.debugElement.nativeElement.querySelector('#login');
+    button.click();
+    fixture.whenStable().then(() => {
+      expect(loginSpy).toHaveBeenCalled();
+    });
+  }));
+
+  it('should call logout', async(() => {
+    const logoutSpy = spyOn(loginService, 'logout').and.stub();
+    const subscription = loginService.login().subscribe(() => {
+      fixture.detectChanges();
+      const button = fixture.debugElement.nativeElement.querySelector('#logout');
+      button.click();
+      fixture.whenStable().then(() => {
+        expect(logoutSpy).toHaveBeenCalled();
+        subscription.unsubscribe();
+      });
+    })
+  }));
 });
